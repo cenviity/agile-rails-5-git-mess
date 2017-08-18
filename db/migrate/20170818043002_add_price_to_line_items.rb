8d9dc563c1c8de71e41c7657e79b37713cd84b4a
Vincent Yim <cenviity@gmail.com>
2017-08-18 04:30:42 +0000

    Add precision and scale to price column; update line items with price

--- db/migrate/20170818043002_add_price_to_line_items.rb
+++ db/migrate/20170818043002_add_price_to_line_items.rb
@@ -1,5 +1,9 @@
 class AddPriceToLineItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :line_items, :price, :decimal
+    add_column :line_items, :price, :decimal, precision: 8, scale: 2
+    
+    LineItem.all.each do |item|
+      item.update(price: item.product.price)
+    end
   end
 end
