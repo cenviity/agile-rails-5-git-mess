9f70ce081411212b29ec6476490a998982c7b449
Vincent Yim <cenviity@gmail.com>
2017-08-18 05:56:14 +0000

    Add tests for adding unique and duplicate products to cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @cart = carts(:empty_cart)
+    @ruby = products(:ruby)
+    @other_product = products(:one)
+  end
+  
+  test "should add unique product as separate line item" do
+    @cart.add_product(@ruby).save!
+    @cart.add_product(@other_product).save!
+    
+    assert_equal 2, @cart.line_items.size
+    assert_equal 59.49, @cart.total_price
+  end
+  
+  test "should add duplicate product to existing line item" do
+    2.times { @cart.add_product(@ruby).save! }
+    
+    assert_equal 1, @cart.line_items.size
+    assert_equal 2, @cart.line_items.first.quantity
+    assert_equal 99, @cart.total_price
+  end
 end
