68b5f39ada45650de105b90da4d8db03c1b47127
Vincent Yim <cenviity@gmail.com>
2017-08-19 15:46:49 +0000

    Add buttons to delete line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,8 @@
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_line_item,
+    only: [:show, :edit, :update, :destroy, :decrement]
 
   # GET /line_items
   # GET /line_items.json
@@ -62,6 +63,17 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+  
+  def decrement
+    respond_to do |format|
+      if @line_item.decrement(:quantity).save
+        format.html { redirect_to store_index_url }
+      else
+        @line_item.destroy
+        format.html { redirect_to store_index_url }
+      end
     end
   end
 
